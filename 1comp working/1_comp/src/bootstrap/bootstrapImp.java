7471b57e7e73457be21e888e8e472c4c6a6349d3
Wayne Nguyen <wayneie_n@hotmail.com>
2017-12-02 20:59:04 -0600

    1 comp working

---
+++ 1comp working/1_comp/src/bootstrap/bootstrapImp.java
@@ -0,0 +1,44 @@
+package bootstrap;
+
+import org.apache.catalina.startup.*;
+
+public class bootstrapImp implements IImp
+{
+	private bootstrapArch _arch;
+
+    public bootstrapImp (){
+    	
+    	Bootstrap b = new Bootstrap();
+    	String[] arg = {"start"};
+    	b.main(arg);
+    	
+    }
+
+	public void setArch(bootstrapArch arch){
+		_arch = arch;
+	}
+	public bootstrapArch getArch(){
+		return _arch;
+	}
+
+	/*
+  	  Myx Lifecycle Methods: these methods are called automatically by the framework
+  	  as the bricks are created, attached, detached, and destroyed respectively.
+	*/	
+	public void init(){
+	    //TODO Auto-generated method stub
+	}
+	public void begin(){
+		//TODO Auto-generated method stub
+	}
+	public void end(){
+		//TODO Auto-generated method stub
+	}
+	public void destroy(){
+		//TODO Auto-generated method stub
+	}
+
+	/*
+  	  Implementation primitives required by the architecture
+	*/
+}
